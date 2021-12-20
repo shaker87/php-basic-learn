c0ddf555adc69dffb479d1639215b3a6041f106f
shaker <shaker.hossain87@gmail.com>
2021-12-20 15:53:15 +0600

    loop and array learn

--- index.php
+++ index.php
@@ -23,11 +23,47 @@ echo $number1 ** $number2 . '<br>';  // 100*100*100*100*100
 function hello_text($value){
     
     echo 'Hello World call from function' .'<br>';
-    for($i = 0; $i <= $value; $i++){
-        echo $i+2 .'<br>';
+    for($i = 1; $i <= $value; $i++){
+        echo $value ,' x ', $i ,' = ', $value * $i  .'<br>';
+        // if($i % 2 == 0){
+        //     echo 'This is even', + $i . '<br>';
+        // }else{
+        //     echo 'This is odd number', + $i .'<br>';
+        // }
     }
    
 }
 
 
 hello_text(10);
+
+$array = ['shaker', 'tarek', 'atik'];
+
+echo $array[0] .'<br>';
+
+for($i = 0; $i < count($array); $i++) {
+    // do something with $array[$i]
+    echo $array[$i] .'<br>';
+}
+
+
+
+// associative array
+
+
+$vehicle = [
+    'brand'=> [
+        'toyota' => [
+            'model'=> ['allion', 'premio', 'ex-corolla'],
+        ]
+        
+     
+    ],
+    'tesla' => 'tesla',
+    
+];
+
+
+// echo $vehicle["brand"]["toyota"]["model"][0];
+
+echo $vehicle["tesla"];
